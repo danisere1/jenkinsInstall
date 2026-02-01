0b3d8fb7547160315d6294cf810bbb4edd12eaf6
danisere1 <danielserfer10@gmail.com>
2026-02-01 21:01:34 +0100

    Arreglo jenkins y sonar para despliegue automatico

--- stopService.sh
+++ stopService.sh
@@ -12,7 +12,22 @@ echo "🛑 Deteniendo Localstack..."
 kill "$(cat localstack_port_forward.pid)"
 rm localstack_port_forward.pid
 
+echo "🛑 Deteniendo SonarQube..."
+
+kill "$(cat sonarqube_port_forward.pid)"
+rm sonarqube_port_forward.pid
+
+echo "🛑 Deteniendo Grafana..."
+
+kill "$(cat grafana_port_forward.pid)"
+rm grafana_port_forward.pid
+
+echo "🛑 Deteniendo Prometheus..."
+
+kill "$(cat prometheus_port_forward.pid)"
+rm prometheus_port_forward.pid
+
 echo "🛑 Deteniendo Minikube..."
 minikube stop
 
-echo "✅ Jenkins y Minikube detenidos correctamente."
+echo "✅ Entorno detenido correctamente."
